ca8cf65a18f2f7b815386be2262f4a5399619e43
Ben Agnew <ba2005@hw.ac.uk>
2024-02-13 18:12:06 +0000

    Fix capitalisation of CharField

--- api/prod/models.py
+++ api/prod/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class Account(models.Model):
-    username = models.charField("username", max_length=30, primary_key=True)
-    display_name = models.charField("display_name", max_length=80)
-    password = models.charField("password", max_length=200)
-    biography = models.charField("biography", max_length=600, default=None)
-    profile_img_path = models.charField("profile_img_path", max_length=1024)
+    username = models.CharField("username", max_length=30, primary_key=True)
+    display_name = models.CharField("display_name", max_length=80)
+    password = models.CharField("password", max_length=200)
+    biography = models.CharField("biography", max_length=600, default=None)
+    profile_img_path = models.CharField("profile_img_path", max_length=1024)
 
 
 class TaskList(models.Model):
     list_id = models.AutoField("list_id", primary_key=True)
-    title = models.charField("title", max_length=80)
-    icon_name = models.charField("icon_name", max_length=100, default=None)
+    title = models.CharField("title", max_length=80)
+    icon_name = models.CharField("icon_name", max_length=100, default=None)
     owner = models.ForeignKey(Account, on_delete=models.CASCADE)
-    sort_by = models.charField("sort_by", max_length=30)
+    sort_by = models.CharField("sort_by", max_length=30)
     visibility = models.PositiveSmallIntegerField("visibility", default=0)
 
 
 class TaskItem(models.Model):
     item_id = models.AutoField("item_id", primary_key=True)
-    body_text = models.charField("body_text", max_length=1500, default=None)
-    remind_method = models.charField("remind_method", max_length=30)
-    attachment_img_path = models.charField("attachment_img_path", max_length=1024, default=None)
+    body_text = models.CharField("body_text", max_length=1500, default=None)
+    remind_method = models.CharField("remind_method", max_length=30)
+    attachment_img_path = models.CharField("attachment_img_path", max_length=1024, default=None)
     snooze_until = models.DateTimeField("snooze_until", default=None)
     completed = models.BooleanField("completed", default=False)
     due_at = models.DateTimeField("due_at")
@@ -52,7 +52,7 @@ class ListSharedWith(models.Model):
     
 class TaskReminder(models.Model):
     task_item = models.ForeignKey(TaskItem, on_delete=models.CASCADE)
-    reminder_type = models.charField("reminder_type", max_length=30)
+    reminder_type = models.CharField("reminder_type", max_length=30)
     reminder_datetime = models.DateTimeField("reminder_datetime", default=None)
     # location_category
     # business_descriptor
