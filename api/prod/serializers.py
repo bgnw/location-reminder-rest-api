f559b76c8f1e865dc385adf38125908dffc481df
Ben Agnew <ba2005@hw.ac.uk>
2024-02-29 19:52:42 +0000

    Add changes to serializers

--- api/prod/serializers.py
+++ api/prod/serializers.py
@@ -9,6 +9,14 @@ class AccountSerializer(serializers.ModelSerializer):
 
 
 class TaskListSerializer(serializers.ModelSerializer):
+	remind_method = serializers.CharField("remind_method", max_length=30, required = False, allow_null = True)
+	attachment_img_path = serializers.CharField("attachment_img_path", max_length=1024, default=None, required = False, allow_null = True)
+	snooze_until = serializers.DateTimeField("snooze_until", default=None, required = False, allow_null = True)
+	completed = serializers.BooleanField("completed", default=False, required = False, allow_null = True)
+	due_at = serializers.DateTimeField("due_at", default=None, required = False, allow_null = True)
+	is_sub_task = serializers.BooleanField("is_sub_task", default=False, required = False, allow_null = True)
+	parent_task = serializers.ForeignKey("self", default=None,
+									required = False, allow_null = True)  # FK of another TaskItem
 	class Meta:
 		model = TaskList
 		fields = ['list_id','title','icon_name','created_at','owner','sort_by','visibility']
