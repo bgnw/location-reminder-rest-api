b2ddb7c974f4972524655d4e770b3d30ee68203b
Ben Agnew <ba2005@hw.ac.uk>
2024-02-29 19:41:12 +0000

    Update TaskItem model to allow nulls

--- api/prod/models.py
+++ api/prod/models.py
@@ -22,13 +22,13 @@ class TaskList(models.Model):
 class TaskItem(models.Model):
     item_id = models.AutoField("item_id", primary_key=True)
     body_text = models.CharField("body_text", max_length=1500, default=None)
-    remind_method = models.CharField("remind_method", max_length=30)
-    attachment_img_path = models.CharField("attachment_img_path", max_length=1024, default=None)
-    snooze_until = models.DateTimeField("snooze_until", default=None)
+    remind_method = models.CharField("remind_method", max_length=30, null=True)
+    attachment_img_path = models.CharField("attachment_img_path", max_length=1024, default=None, null=True)
+    snooze_until = models.DateTimeField("snooze_until", default=None, null=True)
     completed = models.BooleanField("completed", default=False)
-    due_at = models.DateTimeField("due_at")
+    due_at = models.DateTimeField("due_at", null=True)
     is_sub_task = models.BooleanField("is_sub_task", default=False)
-    parent_task = models.ForeignKey("self", on_delete=models.CASCADE, default=None)  # FK of another TaskItem
+    parent_task = models.ForeignKey("self", on_delete=models.CASCADE, default=None, null=True)  # FK of another TaskItem
 
 
 class CollaboratorPendingRequest(models.Model):
