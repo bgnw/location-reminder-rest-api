b6451c334e48cc508dbb767984434e87cb2c84f7
Ben Agnew <ba2005@hw.ac.uk>
2024-03-01 16:47:52 +0000

    Update urls

--- api/prod/accountUrls.py
+++ api/prod/accountUrls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
-from .views import AccountCreate, AccountList, AccountDetail, AccountUpdate, AccountDelete
+from .views import AccountCreate, AccountList, AccountDetail, AccountUpdate, AccountDelete, AccountCheck
 
 urlpatterns = [
     path('create/', AccountCreate.as_view(), name='create-account'),
     path('get', AccountList.as_view()),
     path('<str:pk>', AccountDetail.as_view(), name='retrieve-account'),
+    path('<str:username>\\<str:password>', AccountCheck.as_view(), name='check-account'),
     path('update/<str:pk>', AccountUpdate.as_view(), name='update-account'),
     path('delete/<str:pk>', AccountDelete.as_view(), name='delete-account'),
 ]
