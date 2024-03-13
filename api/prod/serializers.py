f680ca6ca8ecc7cf056f28e74a40a5d98f878fa9
Ben Agnew <ba2005@hw.ac.uk>
2024-03-13 01:37:47 +0000

    Update serializers, incl all fields

--- api/prod/serializers.py
+++ api/prod/serializers.py
@@ -5,22 +5,20 @@ from .models import Account, TaskList, TaskItem, ItemOpportunity
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['username', 'display_name', 'password', 'biography', 'profile_img_path']
+        fields = '__all__'
 
 
 class TaskListSerializer(serializers.ModelSerializer):
     class Meta:
         model = TaskList
-        fields = ['list_id', 'title', 'icon_name', 'created_at', 'owner', 'sort_by', 'visibility']
+        fields = '__all__'
 
 
 class TaskItemSerializer(serializers.ModelSerializer):
     list = TaskListSerializer
     class Meta:
         model = TaskItem
-        fields = ['item_id', 'list', 'owner', 'body_text', 'remind_method', 'poi_filters', 'attachment_img_path',
-                  'snooze_until', 'completed', 'due_at', 'is_sub_task', 'parent_task']
-
+        fields = '__all__'
 
 # class TaskItemMiniSerializer(serializers.ModelSerializer):
 #     class Meta:
