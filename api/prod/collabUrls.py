2db9cdf4179f2dd7ada2fc801b599bf0d1ed7938
Ben Agnew <ba2005@hw.ac.uk>
2024-03-26 22:29:35 +0000

    Add rq/ prefix to some urls

--- api/prod/collabUrls.py
+++ api/prod/collabUrls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('rq/add/', CollabRqAdd.as_view(), name='collab-rq-add'),
     path('rq/delete/<str:pk>', CollabRqDelete.as_view(), name='collab-rq-delete'),
-    path('get-sent/<str:user_master>', CollabRqSentForUser.as_view(), name='collab-rq-sent-for-user'),
-    path('get-received/<str:user_master>', CollabRqReceivedForUser.as_view(), name='collab-rq-recv-for-user'),
+    path('rqget-sent/<str:user_master>', CollabRqSentForUser.as_view(), name='collab-rq-sent-for-user'),
+    path('rq/get-received/<str:user_master>', CollabRqReceivedForUser.as_view(), name='collab-rq-recv-for-user'),
     path('rq/get', CollabRqList.as_view()),
 
     path('add/', CollaboratorAdd.as_view(), name='collab-add'),
