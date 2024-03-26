e1cfd671539e0dcbe98da95351b3797a2a7b054e
Ben Agnew <ba2005@hw.ac.uk>
2024-03-26 22:11:48 +0000

    Add pk path variables

--- api/prod/collabUrls.py
+++ api/prod/collabUrls.py
@@ -3,13 +3,13 @@ from .views import *
 
 urlpatterns = [
     path('rq/add/', CollabRqAdd.as_view(), name='collab-rq-add'),
-    path('rq/delete/', CollabRqDelete.as_view(), name='collab-rq-delete'),
+    path('rq/delete/<str:pk>', CollabRqDelete.as_view(), name='collab-rq-delete'),
     path('get-sent/<str:user_master>', CollabRqSentForUser.as_view(), name='collab-rq-sent-for-user'),
     path('get-received/<str:user_master>', CollabRqReceivedForUser.as_view(), name='collab-rq-recv-for-user'),
     path('rq/get', CollabRqList.as_view()),
 
     path('add/', CollaboratorAdd.as_view(), name='collab-add'),
-    path('delete/', CollaboratorDelete.as_view(), name='collab-delete'),
+    path('delete/<str:pk>', CollaboratorDelete.as_view(), name='collab-delete'),
     path('get-user/<str:user_master>', CollaboratorsForUser.as_view(), name='collab-get-for-user'),
     path('get', CollaboratorList.as_view()),
 ]
